3fad65d47b9320fbf1fdb594012225f17ec55e95
zoobereq <simon@zuberek.net>
2022-06-06 06:57:36 -0400

    Isolates the transcription rules

--- transcribe.py
+++ transcribe.py
@@ -4,17 +4,17 @@
 
 import pynini
 from pynini.lib import rewrite
-from g2g_rules import G2G
+from g2g_rules import G2G_transcribe
 
 
-def ukr2pl(ukrainian_string):
-    polonized_string = rewrite.one_top_rewrite(ukrainian_string, G2G)
+def ukr2pl_transcribe(ukrainian_string):
+    polonized_string = rewrite.one_top_rewrite(ukrainian_string, G2G_transcribe)
     return polonized_string
 
 def main():
     ukr_name = input("Enter the Ukrainian name(s):  ")
-    pl_name = ukr2pl(ukr_name)
-    print(f"The transcribed Polish name(s): {pl_name}")
+    pl_name_transcribed = ukr2pl_transcribe(ukr_name)
+    print(f"The transcribed Polish name(s): {pl_name_transcribed}")
 
 if __name__ == "__main__":
     main()
